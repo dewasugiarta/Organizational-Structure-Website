50a89905f405dcf936aa1e63ff265c769e9b87e9
dewasugiarta <35513925+dewasugiarta@users.noreply.github.com>
2018-04-19 15:42:19 +0800

    dewa

---
+++ process/signupProcess.php
@@ -0,0 +1,48 @@
+<?php
+	if(isset($_POST['submit'])){
+		include_once('../config/dbcon.php');
+		
+		$nama = $_POST['nama'];
+		$jabatan = $_POST['jabatan'];
+		$alamat = $_POST['alamat'];
+		$kabupaten = $_POST['kabupaten'];
+		$kecamatan = $_POST['kecamatan'];
+		$desa = $_POST['desa'];
+		$telepon = $_POST['telepon'];
+		$username = $_POST['username'];
+		$password = $_POST['password'];
+		
+		//echo $nama.','.$jabatan.','.$alamat.','.$kabupaten.','.$kecamatan.','.$desa.','.$telepon.','.$username.','.$password;
+		//check data
+		if(empty($nama) || empty($jabatan) || empty($alamat) || empty($kabupaten) || empty($kecamatan) || empty($desa)||empty($telepon) || empty($username) || empty($password)){
+			header("Location: ../pages/signup.php?signup=empty");
+			exit();
+		}else{
+			$sql = "SELECT * FROM pegawai WHERE userName='$username'";
+			$result = mysqli_query($conn, $sql);
+			$checkResult = mysqli_num_rows($result);
+
+			if($checkResult > 0){
+				header("Location: ../pages/signup.php?signup=usernametaken");
+				exit();
+			}else{
+				$hashedPass = password_hash($password, PASSWORD_DEFAULT);
+				
+				//insert user to database
+				$sql = "INSERT INTO pegawai ".
+				"(nama, jabatan, alamat, kabupaten, kecamatan, desa, telepon, userName, userPass)".
+				"VALUE ('$nama','$jabatan','$alamat','$kabupaten','$kecamatan','$desa','$telepon','$username','$hashedPass')";
+				$insert = mysqli_query($conn, $sql);
+				
+				if($insert){
+					echo "<script> alert('simpan data sukses');location.href='../pages/signup.php';</script>";
+				}else{
+					echo "<script> alert('simpan data gagal');location.href='../pages/signup.php';</script>";
+				}
+			}
+		}
+	}else{
+		header("Location: ../pages/signup.php");
+		exit();
+	}
+?>
